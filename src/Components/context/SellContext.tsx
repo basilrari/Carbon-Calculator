6d0e831c174d59e7aef0c75c1da62db75221346f
allenbiji <georgeallen378@gmail.com>
2024-12-23 18:51:03 +0530

    sell context created

---
+++ src/Components/context/SellContext.tsx
@@ -0,0 +1,39 @@
+import { createContext,useContext } from "react";
+import { useState } from "react";
+
+interface ContextProviderProps{
+      children : React.ReactNode
+}
+
+interface AggregatedData {
+      totalQuantity : number,
+      totalPrice : number, 
+      selectedCount : number
+}  
+
+interface AssetContextType {
+      currentAsset: AggregatedData | null;
+      setCurrentAsset: (asset: AggregatedData) => void;
+    }
+
+const sellContext = createContext<AssetContextType | undefined>(undefined)
+
+export const sellContextProvider : React.FC<ContextProviderProps> = ({children}) => {
+      const [currentAsset, setCurrentAsset] = useState<AggregatedData | null>(null);
+
+      return (
+      <sellContext.Provider value={{ currentAsset, setCurrentAsset }}>
+            {children}
+      </sellContext.Provider>
+      );
+};
+
+
+export const useSellContext = () => {
+      const  data = useContext(sellContext)
+       if (data === undefined){
+        console.log("provider not wrapped")
+       }
+
+       return data;
+}
