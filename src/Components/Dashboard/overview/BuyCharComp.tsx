0b3c94edfeec13d033c310dc69b1f376e03bf57e
allenbiji <georgeallen378@gmail.com>
2025-02-16 19:51:56 +0530

    dashboard comp ui mod

--- src/Components/Dashboard/overview/BuyCharComp.tsx
+++ src/Components/Dashboard/overview/BuyCharComp.tsx
@@ -8,7 +8,7 @@ const BuyCharComp = () => {
                 <p className="text-lg font-medium">Offsetting Your Emissions With Decarb</p>
 
                 <Link href="/decarb/contracts/sellassets">
-                <button className="bg-[#b1b1b1] text-white font-medium px-4 py-2 w-1/2 rounded-md shadow-md">Sell CHAR</button>
+                <button className="bg-[#b1b1b1] text-white hover:bg-[#9e9f9f] transition-colors duration-300 font-medium px-4 py-2 w-1/2 rounded-md shadow-md">Sell CHAR</button>
                 </Link>
 
               </div>
@@ -17,7 +17,7 @@ const BuyCharComp = () => {
                 <p className="text-lg font-medium">Accessing The Sustainable World With Decarb</p>
 
                 <Link href="/decarb/contracts/buyassets">
-                    <button className="bg-[#366E6A] text-white font-medium px-4 py-2 w-1/2 rounded-md shadow-md">Buy CHAR</button>
+                    <button className="bg-[#366E6A] text-white hover:bg-[#3c6262] transition-colors duration-300 font-medium px-4 py-2 w-1/2 rounded-md shadow-md">Buy CHAR</button>
                 </Link>
 
                 </div> 
--- src/Components/Dashboard/overview/DiscoverComp.tsx
+++ src/Components/Dashboard/overview/DiscoverComp.tsx
@@ -7,7 +7,7 @@ const DiscoverComp = () => {
             <p className="text-lg font-semibold text-[#2F4F4F]">Taking Leaps Toward Sustainability With Decarb</p>
 
             <Link href="/decarb/discover">
-               <button className="bg-[#d9e4e7] text-black px-4 py-2 rounded-md shadow-md absolute right-4 bottom-4">Discover</button>
+               <button className="bg-[#d9e4e7] text-black px-4 py-2 rounded-md shadow-md hover:bg-[#ccdede] transition-colors duration-300 absolute right-4 bottom-4">Discover</button>
             </Link>
             
         </div>
